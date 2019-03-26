482789141972b0e17a8a9bc3cbc4276d97627c7e
Francesco Sullo <sullof@gmail.com>
2019-03-26 13:13:40 -0700

    Updating settings for TronBox

--- tronbox.js
+++ tronbox.js
@@ -1,61 +1,39 @@
+const port = process.env.HOST_PORT || 9090
+
 module.exports = {
   networks: {
-    development: {
-// For trontools/quickstart docker image
-      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
-      consume_user_resource_percent: 30,
-      fee_limit: 100000000,
-
-      // Requires TronBox 2.1.9+ and Tron Quickstart 1.1.16+
-      // fullHost: "http://127.0.0.1:9090",
-
-      // The three settings below for TronBox < 2.1.9
-      fullNode: "http://127.0.0.1:9090",
-      solidityNode: "http://127.0.0.1:9090",
-      eventServer: "http://127.0.0.1:9090",
-
-      network_id: "*"
-    },
     mainnet: {
-// Don't put your private key here:
-      privateKey: process.env.PK,
+      // Don't put your private key here:
+      privateKey: process.env.PRIVATE_KEY_MAINNET,
       /*
-      Create a .env file (it must be gitignored) containing something like
+Create a .env file (it must be gitignored) containing something like
 
-        export PK=4E7FECCB71207B867C495B51A9758B104B1D4422088A87F4978BE64636656243
+  export PRIVATE_KEY_MAINNET=4E7FECCB71207B867C495B51A9758B104B1D4422088A87F4978BE64636656243
 
-      Then, run the migration with:
+Then, run the migration with:
 
-        source .env && tronbox migrate --network mainnet
+  source .env && tronbox migrate --network mainnet
 
-      */
-      consume_user_resource_percent: 30,
-      fee_limit: 100000000,
-
-      // tronbox 2.1.9+
-      // fullHost: "https://api.trongrid.io",
-
-      // tronbox < 2.1.9
-      fullNode: "https://api.trongrid.io",
-      solidityNode: "https://api.trongrid.io",
-      eventServer: "https://api.trongrid.io",
-
-      network_id: "*"
+*/
+      userFeePercentage: 100,
+      feeLimit: 1e8,
+      fullHost: "https://api.trongrid.io",
+      network_id: "1"
     },
     shasta: {
-      privateKey: process.env.PK,
-      consume_user_resource_percent: 30,
-      fee_limit: 100000000,
-
-      // tronbox 2.1.9+
-      // fullHost: "https://api.shasta.trongrid.io",
-
-      // tronbox < 2.1.9
-      fullNode: "https://api.shasta.trongrid.io",
-      solidityNode: "https://api.shasta.trongrid.io",
-      eventServer: "https://api.shasta.trongrid.io",
-
-      network_id: "*"
+      privateKey: process.env.PRIVATE_KEY_SHASTA,
+      userFeePercentage: 50,
+      feeLimit: 1e8,
+      fullHost: "https://api.shasta.trongrid.io",
+      network_id: "2"
+    },
+    development: {
+      // For trontools/quickstart docker image
+      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+      userFeePercentage: 50,
+      feeLimit: 1e8,
+      fullHost: 'http://127.0.0.1:' + port,
+      network_id: "9"
     }
   }
 }
